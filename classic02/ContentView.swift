9b49080f859ac4aa3044b6339fe8a7658561eac7
kb18k <jackexi@gmail.com>
2021-09-03 21:25:05 +0800

    增加版面Text元件 畫面排版

--- classic02/ContentView.swift
+++ classic02/ContentView.swift
@@ -9,8 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("學習如何Git版控")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .padding(.top,40)
+            Spacer()//填充空白
+            Text("好好學會版控日後方便很多")
+                .padding(.bottom,40)
+        }
+        .frame(height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        .frame(maxWidth:.infinity)
+        .background(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)))
+        .cornerRadius(16)
+        .padding()
     }
 }
 
