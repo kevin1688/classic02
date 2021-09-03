bff3ee52ec6b2b01cc2ab2457b79400b96419fad
kb18k <jackexi@gmail.com>
2021-09-03 21:00:56 +0800

    add git commit

---
+++ classic02/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  classic02
+//
+//  Created by kai wen chen on 2021/9/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ classic02/classic02App.swift
@@ -0,0 +1,17 @@
+//
+//  classic02App.swift
+//  classic02
+//
+//  Created by kai wen chen on 2021/9/3.
+//
+
+import SwiftUI
+
+@main
+struct classic02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
